35e4a1b73253b92a74fd74394ec7d05ec12361ef
Nathan <63520059+shqk@users.noreply.github.com>
2026-04-12 19:56:52 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -19,31 +19,55 @@ def main():
     # client_socket.send(bytes("HTTP/1.1 200 OK\r\n\r\n", "utf-8"))
 
     # Extract URL path
-    data = client_socket.recv(1024)
-    request_line = data.split(b"\r\n")
+
+    if (data := client_socket.recv(1024)) and (b"\r\n\r\n" in data):
+        request_line_headers, body = data.split(b"\r\n\r\n", maxsplit=1)
+
+        print("request_line_headers: ", request_line_headers)
+        print("Body: ", body)
+
+        request_line = b''
+
+        if request_line_headers:
+            request_line, *headers = request_line_headers.split(b"\r\n")
+
+        print("Only request line: ", request_line)
+        print("Only headers: ",headers)
 
     # Récupérer le chemin URL => deuxième élement d'une requête
 
-    if data and len(request_line) > 1:
-        url_path = data.split(b" ")[1]
-        
-        if url_path == b"/":
-            client_socket.send(bytes("HTTP/1.1 200 OK\r\n\r\n", "utf-8"))
+    if request_line:
+    
+        request_line_splitted = request_line.split(b" ")
+        headers_dic = {} 
 
-        elif url_path.startswith(b"/echo/"):
-            to_echo = url_path[len(b"/echo/"):]
-            print(to_echo)
-            client_socket.send(bytes(f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(to_echo)}\r\n\r\n{to_echo.decode("utf-8")}", "utf-8"))
+        if headers:
+            # Utilisation du max_split pour split au premier ":"
+            for header in headers:
+                k, v = header.split(b':', maxsplit=1)
+                headers_dic[k.lower()] = v.strip()
+        print("Headers dictionnary: ", headers_dic)
+
+        if len(request_line_splitted) == 3:
+            method, url_path, version = request_line_splitted
         
-        else:
-            client_socket.send(bytes("HTTP/1.1 404 Not Found\r\n\r\n", "utf-8"))
+            if url_path == b"/":
+                client_socket.send(bytes("HTTP/1.1 200 OK\r\n\r\n", "utf-8"))
+
+            elif url_path == b"/user-agent":
+                if headers_dic[b"user-agent"]:
+                    client_socket.send(bytes(f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(headers_dic[b"user-agent"])}\r\n\r\n{headers_dic[b"user-agent"].decode("utf-8")}", "utf-8"))
+
+            elif url_path.startswith(b"/echo/"):
+                to_echo = url_path[len(b"/echo/"):]
+                client_socket.send(bytes(f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(to_echo)}\r\n\r\n{to_echo.decode("utf-8")}", "utf-8"))
+            
+            else:
+                client_socket.send(bytes("HTTP/1.1 404 Not Found\r\n\r\n", "utf-8"))
 
 
     if not data:
         response = client_socket.send(bytes("HTTP/1.1 404 Not Found\r\n\r\n", "utf-8"))
-
-    # print("Data decoded: ", data.decode("utf-8", errors="replace"))
-    print("Data: ", data)
 
     client_socket.send(bytes("HTTP/1.1 200 OK\r\n\r\n", "utf-8"))
 
