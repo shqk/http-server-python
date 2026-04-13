a6bb92a9774ac8adb2cb06a455764a53e8ffb2b1
Nathan <63520059+shqk@users.noreply.github.com>
2026-04-13 18:28:09 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,26 +1,8 @@
 import socket  # noqa: F401
 import re
+import threading
 
-
-def main():
-
-    # server_socket écoute les demandes de connexions entrantes (une sorte de réceptionniste)
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-
-    # Ici le serveur accepte la connexion et alloue un canal dédié avec .accept stocké dans le socket_client
-    # (avec par lequel la communication va s'effectuer ; lien entre le socket du navigateur et un socket créé par le serveur en conséquence pour communiquer)
-    client_socket, client_adress = server_socket.accept()  # wait for client
-
-    print("Server socket: ", server_socket)
-    print("Client socket: ", client_socket)
-    print("Client address: ", client_adress)
-
-    # Server Response Status 200 OK
-    # client_socket.sendall(bytes("HTTP/1.1 200 OK\r\n\r\n", "utf-8"))
-
-    # Extract URL path
-
-    
+def handle_client(client_socket : socket.socket) -> None:
     status_line = b"HTTP/1.1 404 Not Found\r\n"
     request_headers_list = []
     request_headers = {}
@@ -44,7 +26,6 @@ def main():
         print("Only request line: ", request_line)
         print("Only headers: ", request_headers_list)
 
-    # Récupérer le chemin URL => deuxième élement d'une requête
 
     if request_line:
     
@@ -85,6 +66,20 @@ def main():
             
         response = status_line + b"\r\n" + response_headers + b"\r\n" + response_body
         client_socket.sendall(response)
+
+
+def main():
+
+    # server_socket écoute les demandes de connexions entrantes (une sorte de réceptionniste)
+    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+
+    # Ici le serveur accepte la connexion et alloue un canal dédié avec .accept stocké dans le socket_client
+    # (avec par lequel la communication va s'effectuer ; lien entre le socket du navigateur et un socket créé par le serveur en conséquence pour communiquer)
+    while True:
+        client_socket, client_adress = server_socket.accept()  # wait for client
+        print("Client address: ", client_adress)
+        threading.Thread(target=handle_client, args=(client_socket,)).start()
+
 
 
 
