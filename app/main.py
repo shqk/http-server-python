93e7e794a2d7aec1d0ab592af05c59799d0845ed
Nathan <63520059+shqk@users.noreply.github.com>
2026-04-20 08:43:54 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 import socket  # noqa: F401
 import re
 import threading
+import argparse
+import os
 
-def handle_client(client_socket : socket.socket) -> None:
+def handle_client(client_socket : socket.socket, file_folder: str | None = None) -> None:
     status_line = b"HTTP/1.1 404 Not Found\r\n"
     request_headers_list = []
     request_headers = {}
@@ -64,6 +66,27 @@ def handle_client(client_socket : socket.socket) -> None:
                 response_headers = b"Content-Type: text/plain\r\n" + b"Content-Length: " + str(len(to_echo)).encode("ascii") + b"\r\n"
                 response_body = to_echo
             
+            elif url_path.startswith(b"/files/") and file_folder:
+                file_name = url_path[len(b"/files/"):].decode("utf-8")
+                print("directory", os.getcwd())
+                print(f"Est-ce que ca existe ?: file folder : {file_folder} - file_name: {file_name}", os.path.exists(f"{file_folder}{file_name}"))
+                if os.path.exists(f"{file_folder}{file_name}"):
+                    status_line = b"HTTP/1.1 200 OK"
+                    size = os.path.getsize(file_folder + file_name)
+                    
+                    print("Filename: ", file_name)
+                    print("Size: ", size)
+
+
+                    response_headers = b"Content-Type: application/octet-stream\r\n" + b"Content-Length: " + str(size).encode("ascii") + b"\r\n"
+
+                    with open(f"{file_folder}{file_name}", "r") as file:
+                        content = file.read()
+                    
+                    response_body = content.encode("utf-8")
+                    print("Content: ", response_body)
+
+            
         response = status_line + b"\r\n" + response_headers + b"\r\n" + response_body
         client_socket.sendall(response)
 
@@ -73,12 +96,16 @@ def main():
     # server_socket écoute les demandes de connexions entrantes (une sorte de réceptionniste)
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
 
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--directory', type=str)
+    args = parser.parse_args()
+
     # Ici le serveur accepte la connexion et alloue un canal dédié avec .accept stocké dans le socket_client
     # (avec par lequel la communication va s'effectuer ; lien entre le socket du navigateur et un socket créé par le serveur en conséquence pour communiquer)
     while True:
         client_socket, client_adress = server_socket.accept()  # wait for client
         print("Client address: ", client_adress)
-        threading.Thread(target=handle_client, args=(client_socket,)).start()
+        threading.Thread(target=handle_client, args=(client_socket, args.directory)).start()
 
 
 
